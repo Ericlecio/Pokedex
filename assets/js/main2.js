7c966552613b74bc90579be776043a8f0cfde0cf
Ericlecio <ericleciojr14@gmail.com>
2023-03-12 13:58:39 -0300

    .

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -69,3 +69,74 @@ loadMoreD.addEventListener('click', () => {
     offset += limit
     loadPokemonD(offset, limit)
 })
+
+
+
+
+const btn1 = document.getElementById('botao1');
+const tipo1 = document.getElementById('tipo1');
+
+const btn2 = document.getElementById('botao2'); 
+const tipo2 = document.getElementById('tipo2');
+
+const btn3 = document.getElementById('botao3');
+const tipo3 = document.getElementById('tipo3');
+
+const btn4 = document.getElementById('botao4');
+const tipo4 = document.getElementById('tipo4');
+
+btn1.addEventListener('click', function () {
+
+    if (tipo1.style.display === 'block') {
+        tipo1.style.display = 'block';
+
+    } else {
+        tipo1.style.display = 'block';
+        tipo2.style.display = 'none';
+        tipo3.style.display = 'none';
+        tipo4.style.display = 'none';
+    }
+
+});
+
+btn2.addEventListener('click', function () {
+
+    if (tipo2.style.display === 'block') {
+        tipo2.style.display = 'none';
+
+    } else {
+        tipo1.style.display = 'none';
+        tipo2.style.display = 'block';
+        tipo3.style.display = 'none';
+        tipo4.style.display = 'none';
+    }
+
+});
+
+btn3.addEventListener('click', function () {
+
+    if (tipo3.style.display === 'block') {
+        tipo3.style.display = 'none';
+
+    } else {
+        tipo1.style.display = 'none';
+        tipo2.style.display = 'none';
+        tipo3.style.display = 'block';
+        tipo4.style.display = 'none';
+    }
+
+});
+
+btn4.addEventListener('click', function () {
+
+    if (tipo4.style.display === 'block') {
+        tipo4.style.display = 'none';
+
+    } else {
+        tipo1.style.display = 'none';
+        tipo2.style.display = 'none';
+        tipo3.style.display = 'none';
+        tipo4.style.display = 'block';
+    }
+
+});
